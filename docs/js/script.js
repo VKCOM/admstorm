ddff1d63657924cb68777fde88648011084e730e
Makhnev Petr <51853996+i582@users.noreply.github.com>
2022-02-19 01:41:43 +0300

    landing: added whats new for 1.1.0 (#40)

--- docs/js/script.js
+++ docs/js/script.js
@@ -25,4 +25,17 @@ window.onload = () => {
       el.src = el.src.replace(".png", `.${serverName}.png`);
     });
   }
+
+  const securityVideos = document.querySelectorAll(".js-security-video");
+  securityVideos.forEach((el) => {
+    const src = el.dataset.src;
+
+    if (serverName !== defaultServerName) {
+      el.innerHTML = `<video class="chapter__version-block-new-feature__video" controls="controls" preload="auto">
+    <source src="${src}.${serverName}.mp4" type='video/mp4;"'>
+</video>`;
+    } else {
+      el.innerHTML = `<img class="chapter__version-block-new-feature__image" src="${src}.png" alt=""/>`;
+    }
+  });
 };
