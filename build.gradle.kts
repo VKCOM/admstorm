7b9705b33ffeeba264c433b5a2aa3c75fe8bf8bd
Danil Ovchinnikov <danil42worker@gmail.com>
2022-06-20 01:54:04 +0300

    added a file for saving parameters when building

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import io.gitlab.arturbosch.detekt.Detekt
+import org.apache.tools.ant.filters.ReplaceTokens
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key).toString()
@@ -71,6 +72,17 @@ detekt {
 }
 
 tasks {
+    processResources {
+        val tokens = mapOf(
+            "sentry_dsn" to System.getenv("SENTRY_DSN"),
+        )
+
+        filesMatching("plugin_config.json") {
+            filteringCharset = "UTF-8"
+            filter<ReplaceTokens>("tokens" to tokens)
+        }
+    }
+
     properties("javaVersion").let {
         withType<JavaCompile> {
             sourceCompatibility = it
