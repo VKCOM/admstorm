05fe744742eb3f564a7aa5e47e230aef5399c43c
i582 <51853996+i582@users.noreply.github.com>
2022-06-22 15:59:29 +0300

    updated

--- src/main/kotlin/com/vk/admstorm/actions/SendLogsToSentryAction.kt
+++ src/main/kotlin/com/vk/admstorm/actions/SendLogsToSentryAction.kt
@@ -3,14 +3,19 @@ package com.vk.admstorm.actions
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.vk.admstorm.notifications.AdmNotification
 import com.vk.admstorm.services.SentryService
+import com.vk.admstorm.utils.MyUtils
 
 class SendLogsToSentryAction : AdmActionBase() {
     override fun actionWithConnectionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
-        SentryService.getInstance(project).sendIdeaLog()
+        val sentryId = SentryService.getInstance(project).sendIdeaLog()
 
-        AdmNotification("Thanks for feedback!")
+        AdmNotification("Thanks for feedback! Your unique error number: '$sentryId'")
             .withTitle("Logs successfully sent to Sentry")
+            .withActions(AdmNotification.Action("Copy error ID") { _, notification ->
+                MyUtils.copyToClipboard(sentryId.toString())
+                notification.expire()
+            })
             .show(project)
     }
 }
