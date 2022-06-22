4a77b8700a68895a3bb54586906a1bf03f13e904
Makhnev Petr <51853996+i582@users.noreply.github.com>
2022-06-22 11:36:46 +0300

    treat benchmark classes and methods as always implicit used (#99)

---
+++ src/main/kotlin/com/vk/admstorm/configuration/kbench/KBenchImplicitTypeProvider.kt
@@ -0,0 +1,26 @@
+package com.vk.admstorm.configuration.kbench
+
+import com.intellij.codeInsight.daemon.ImplicitUsageProvider
+import com.intellij.psi.PsiElement
+import com.jetbrains.php.lang.psi.elements.PhpClass
+import com.jetbrains.php.lang.psi.elements.PhpClassMember
+import com.vk.admstorm.utils.KBenchUtils
+
+class KBenchImplicitTypeProvider : ImplicitUsageProvider {
+    override fun isImplicitUsage(element: PsiElement): Boolean {
+        if (element is PhpClass && KBenchUtils.isBenchmarkClass(element)) {
+            return true
+        }
+
+        if (element is PhpClassMember) {
+            val klass = element.containingClass ?: return false
+            return KBenchUtils.isBenchmarkClass(klass)
+        }
+
+        return false
+    }
+
+    override fun isImplicitRead(element: PsiElement) = false
+
+    override fun isImplicitWrite(element: PsiElement) = false
+}
