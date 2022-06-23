b778e48a5173e356dc6ab8d616c64b23754b8726
Makhnev Petr <51853996+i582@users.noreply.github.com>
2022-06-23 20:21:49 +0300

    hid yarn watch for non-vkcom projects (#117)

--- src/main/kotlin/com/vk/admstorm/ui/YarnWatchStatusBarWidgetFactory.kt
+++ src/main/kotlin/com/vk/admstorm/ui/YarnWatchStatusBarWidgetFactory.kt
@@ -4,17 +4,16 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.impl.status.widget.StatusBarEditorBasedWidgetFactory
+import com.vk.admstorm.utils.extensions.pluginEnabled
 
 class YarnWatchStatusBarWidgetFactory : StatusBarEditorBasedWidgetFactory() {
     override fun getId(): String = YarnWatchStatusBarWidget.WIDGET_ID
 
-    override fun getDisplayName(): String {
-        return "Yarn Watch"
-    }
+    override fun getDisplayName() = "Yarn Watch"
 
-    override fun createWidget(project: Project): StatusBarWidget {
-        return YarnWatchStatusBarWidget(project)
-    }
+    override fun isAvailable(project: Project) = project.pluginEnabled()
+
+    override fun createWidget(project: Project) = YarnWatchStatusBarWidget(project)
 
     override fun disposeWidget(widget: StatusBarWidget) {
         Disposer.dispose(widget)
