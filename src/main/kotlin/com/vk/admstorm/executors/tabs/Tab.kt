e46a492e6ca9046ffc7f41e4e06dd9789e7f4fdf
i582 <51853996+i582@users.noreply.github.com>
2022-06-25 14:04:49 +0300

    fixed the bug with tabs

--- src/main/kotlin/com/vk/admstorm/executors/tabs/Tab.kt
+++ src/main/kotlin/com/vk/admstorm/executors/tabs/Tab.kt
@@ -2,7 +2,11 @@ package com.vk.admstorm.executors.tabs
 
 import com.intellij.execution.ui.RunnerLayoutUi
 import com.intellij.ui.content.Content
+import com.vk.admstorm.executors.BaseRemoteExecutor
 
+/**
+ * Base class for all tabs for the [BaseRemoteExecutor].
+ */
 interface Tab {
     val name: String
     val content: Content?
