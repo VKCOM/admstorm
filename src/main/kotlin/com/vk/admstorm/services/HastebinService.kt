1e2fdfc1ce1fa200f77835b89f3aa9763e4ae967
Richardas Kuchinskas <r.kuchinskas@vk.team>
2023-12-12 18:35:27 +0300

    solution prototype

--- src/main/kotlin/com/vk/admstorm/services/HastebinService.kt
+++ src/main/kotlin/com/vk/admstorm/services/HastebinService.kt
@@ -3,8 +3,14 @@ package com.vk.admstorm.services
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.vk.admstorm.CommandRunner
-import com.vk.admstorm.env.Env
+/*import com.vk.admstorm.CommandRunner
+import com.vk.admstorm.env.Env*/
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpResponse
 
 @Service(Service.Level.PROJECT)
 class HastebinService(private val project: Project) {
@@ -14,6 +20,19 @@ class HastebinService(private val project: Project) {
 
     fun createHaste(data: String): String {
         val output = data.replace("\"", "\\\"").replace("$", "\\$")
-        return CommandRunner.runRemotely(project, "echo \"$output\" | ${Env.data.pasteBinCommand}").stdout
+
+        val client = HttpClient.newBuilder().build()
+        val request = java.net.http.HttpRequest.newBuilder().uri(URI.create("secret"))
+            .POST(java.net.http.HttpRequest.BodyPublishers.ofString(output)).build()
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+        val jsonResponse = Json.parseToJsonElement(response.body())
+        if (jsonResponse is JsonObject) {
+            val value = jsonResponse["key"]?.jsonPrimitive?.content
+            return "secret/${value}"
+        }
+        return ""
+        /*
+                return CommandRunner.runRemotely(project, "echo \"$output\" | ${Env.data.pasteBinCommand}").stdout*/
     }
 }
