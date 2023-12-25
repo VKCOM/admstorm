cf2e6b5e1241c094eb2dcd8fbf9c26197c6e20dc
Richardas Kuchinskas <r.kuchinskas@vk.team>
2023-12-25 17:23:04 +0300

    logic for getting internal service

--- src/main/kotlin/com/vk/admstorm/services/HastebinService.kt
+++ src/main/kotlin/com/vk/admstorm/services/HastebinService.kt
@@ -3,8 +3,8 @@ package com.vk.admstorm.services
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-/*import com.vk.admstorm.CommandRunner
-import com.vk.admstorm.env.Env*/
+import com.vk.admstorm.env.Env
+import com.vk.admstorm.env.getByKey
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonPrimitive
@@ -13,26 +13,25 @@ import java.net.http.HttpClient
 import java.net.http.HttpResponse
 
 @Service(Service.Level.PROJECT)
-class HastebinService(private val project: Project) {
+class HastebinService {
     companion object {
         fun getInstance(project: Project) = project.service<HastebinService>()
     }
 
     fun createHaste(data: String): String {
         val output = data.replace("\"", "\\\"").replace("$", "\\$")
+        val hastLink = Env.data.services.getByKey("hastebin")?.url ?: return ""
 
         val client = HttpClient.newBuilder().build()
-        val request = java.net.http.HttpRequest.newBuilder().uri(URI.create("secret"))
+        val request = java.net.http.HttpRequest.newBuilder().uri(URI.create("$hastLink/documents"))
             .POST(java.net.http.HttpRequest.BodyPublishers.ofString(output)).build()
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
 
         val jsonResponse = Json.parseToJsonElement(response.body())
         if (jsonResponse is JsonObject) {
             val value = jsonResponse["key"]?.jsonPrimitive?.content
-            return "secret/${value}"
+            return "$hastLink/${value}"
         }
         return ""
-        /*
-                return CommandRunner.runRemotely(project, "echo \"$output\" | ${Env.data.pasteBinCommand}").stdout*/
     }
 }
