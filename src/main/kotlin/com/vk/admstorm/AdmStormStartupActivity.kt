5e4f9dbc15030a8bb8f182a826ba09edf57ebdcf
Richardas Kuchinskas <36065987+Hidanio@users.noreply.github.com>
2024-02-09 21:47:24 +0300

    notifications about new versions of plugins (#207)
    * added new plugins to AdmService and extended logic of notification
    ---------
    
    Co-authored-by: Danil Ovchinnikov <Danil42Worker@gmail.com>

--- src/main/kotlin/com/vk/admstorm/AdmStormStartupActivity.kt
+++ src/main/kotlin/com/vk/admstorm/AdmStormStartupActivity.kt
@@ -1,25 +1,21 @@
 package com.vk.admstorm
 
-import com.intellij.ide.BrowserUtil
-import com.intellij.ide.plugins.InstalledPluginsState
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileTypes.ex.FileTypeChooser
 import com.intellij.openapi.fileTypes.impl.AbstractFileType
-import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.util.registry.Registry
 import com.vk.admstorm.diagnostic.AdmStormLoggerFactory
 import com.vk.admstorm.highlight.CppTypeHighlightPatcher
-import com.vk.admstorm.notifications.AdmNotification
 import com.vk.admstorm.services.SentryService
 import com.vk.admstorm.settings.AdmStormSettingsState
 import com.vk.admstorm.ssh.SshConnectionService
 import com.vk.admstorm.startup.ChangeSshBackendStartup
+import com.vk.admstorm.startup.PluginsUpdateStartup
 import com.vk.admstorm.utils.MyUtils.measureTime
-import com.vk.admstorm.utils.ServerNameProvider
 import com.vk.admstorm.utils.extensions.pluginEnabled
 
 class AdmStormStartupActivity : ProjectActivity {
@@ -43,7 +39,7 @@ class AdmStormStartupActivity : ProjectActivity {
         }
 
         ChangeSshBackendStartup.changeConfigurationProcess(project)
-        checkUpdates(project)
+        PluginsUpdateStartup.checkUpdates(project)
 
         measureTime(LOG, "patch cpp highlight") {
             val cppType = FileTypeChooser.getKnownFileTypeOrAssociate(".c") as AbstractFileType
@@ -67,28 +63,5 @@ class AdmStormStartupActivity : ProjectActivity {
         val sentry = SentryService.getInstance(project)
 
         Logger.setFactory(AdmStormLoggerFactory(sentry, defaultLoggerFactory))
-    }
-
-    private fun checkUpdates(project: Project) {
-        val hasNewerVersion = InstalledPluginsState.getInstance().hasNewerVersion(AdmService.PLUGIN_ID)
-        if (!hasNewerVersion) {
-            return
-        }
-
-        AdmNotification("New version of the plugin is available")
-            .withActions(
-                AdmNotification.Action("Update") { _, _ ->
-                    invokeLater {
-                        ShowSettingsUtil.getInstance().showSettingsDialog(project, "Plugins")
-                    }
-                }
-            )
-            .withActions(
-                AdmNotification.Action("What's new") { _, _ ->
-                    val url = "https://vkcom.github.io/admstorm/whatsnew.html?server_name=${ServerNameProvider.name()}"
-                    BrowserUtil.browse(url)
-                }
-            )
-            .show(project)
     }
 }
