95eea9e5075d27a0373479310a8d641c62fe9044
Richardas Kuchinskas <36065987+Hidanio@users.noreply.github.com>
2024-05-27 18:00:45 +0300

    changed ssh connection type (#233)

--- src/main/kotlin/com/vk/admstorm/startup/ChangeSshBackendStartup.kt
+++ src/main/kotlin/com/vk/admstorm/startup/ChangeSshBackendStartup.kt
@@ -10,7 +10,8 @@ import com.vk.admstorm.notifications.AdmNotification
 object ChangeSshBackendStartup {
     private val LOG = logger<ChangeSshBackendStartup>()
 
-    private const val SSH_NOTIFICATOR_OPTION = "sshLegacyChanges"
+    private const val LEGACY_SSH_NOTIFICATOR_OPTION = "sshLegacyChanges"
+    private const val SSH_NOTIFICATOR_OPTION = "sshOpenSSHChanges"
     private const val SSH_CONFIG_BACKEND = "ssh.config.backend"
 
     private fun changeSshConfiguration(configValue: SshConnectionConfigService.Kind) {
@@ -18,36 +19,43 @@ object ChangeSshBackendStartup {
     }
 
     fun changeConfigurationProcess(project: Project) {
-        val dntShow = PropertiesComponent.getInstance(project).getBoolean(SSH_NOTIFICATOR_OPTION)
+        val properties = PropertiesComponent.getInstance(project)
+
+        val dntShow = properties.getBoolean(SSH_NOTIFICATOR_OPTION)
         if (dntShow) {
-            LOG.info("LEGACY SSH option was enabled")
+            LOG.info("OPENSSH option was enabled")
             return
+        }
+
+        //TODO: Remove it after 2 month
+        if (properties.isValueSet(LEGACY_SSH_NOTIFICATOR_OPTION)) {
+            properties.unsetValue(LEGACY_SSH_NOTIFICATOR_OPTION)
         }
 
         val sshSettingValue = AdvancedSettings.getEnum(SSH_CONFIG_BACKEND, SshConnectionConfigService.Kind::class.java)
-        if (sshSettingValue == SshConnectionConfigService.Kind.LEGACY) {
-            PropertiesComponent.getInstance(project).setValue(SSH_NOTIFICATOR_OPTION, true)
-            LOG.info("LEGACY SSH option was enabled before")
+        if (sshSettingValue == SshConnectionConfigService.Kind.OPENSSH) {
+            properties.setValue(SSH_NOTIFICATOR_OPTION, true)
+            LOG.info("OPENSSH option was enabled before")
             return
         }
 
-        changeSshConfiguration(SshConnectionConfigService.Kind.LEGACY)
-        LOG.info("LEGACY SSH has been changed")
+        changeSshConfiguration(SshConnectionConfigService.Kind.OPENSSH)
+        LOG.info("LEGACY SSH has been changed to OPENSSH")
 
-        AdmNotification("We changed your ssh type to LEGACY")
+        AdmNotification("We changed your ssh type to OPENSSH")
             .withActions(
                 AdmNotification.Action("Don`t show it again") { _, notification ->
-                    LOG.info("LEGACY SSH option has been set")
+                    LOG.info("OPENSSH option has been set")
                     notification.expire()
                 }
             ).withActions(
                 AdmNotification.Action("Rollback and turn off this notification") { _, notification ->
-                    changeSshConfiguration(SshConnectionConfigService.Kind.OPENSSH)
-                    LOG.info("SSH option changed to OpenSSH")
+                    changeSshConfiguration(SshConnectionConfigService.Kind.LEGACY)
+                    LOG.info("SSH option changed to LEGACY")
                     notification.expire()
                 }
             ).show(project)
 
-        PropertiesComponent.getInstance(project).setValue(SSH_NOTIFICATOR_OPTION, true)
+        properties.setValue(SSH_NOTIFICATOR_OPTION, true)
     }
 }
