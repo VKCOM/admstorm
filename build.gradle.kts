d503fd22df6d42c2eaa37e4c4fbf912407a76b99
Danil Ovchinnikov <Danil42Worker@gmail.com>
2025-12-23 14:24:08 +0300

    remove qodana

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     alias(libs.plugins.kotlin) // Kotlin support
     alias(libs.plugins.intelliJPlatform) // IntelliJ Platform Gradle Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
-    alias(libs.plugins.qodana) // Gradle Qodana Plugin
     alias(libs.plugins.kover) // Gradle Kover Plugin
     alias(libs.plugins.kotlinSerialization) // Kotlinx serialization
     alias(libs.plugins.gradleDetektPlugin) // Gradle Detekt Plugin
